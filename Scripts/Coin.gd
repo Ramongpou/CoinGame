99afa3d8e5a6d9647bc23c1c02b56fe93f651781
Ramon <ramongpou@gmail.com>
2024-08-24 18:30:22 +0200

    Colors Added

--- Scripts/Coin.gd
+++ Scripts/Coin.gd
@@ -1,19 +1,48 @@
 extends RigidBody3D
 
+@onready var ClickTimer : Timer = $"ClickTimer"
+@onready var Animator : AnimationPlayer = $"AnimationPlayer"
+var CanClick : bool
+var force : float = 1
+var StartPosition : Vector3
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	StartPosition = position
+	CanClick = true
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	if(position.x != StartPosition.x):
+		position.x = StartPosition.x
+	if(position.z != StartPosition.z):
+		position.z = StartPosition.z
+	if(rotation_degrees.y != 0):
+			rotation_degrees.y = 0
+	if(rotation_degrees.z != 0):
+			rotation_degrees.z = 0
+	if (angular_velocity.x > 0):
+			angular_velocity.x -=  clamp(angular_velocity.x,0.1,10) * delta
+	if (angular_velocity.x <  0):
+			angular_velocity.x += clamp(angular_velocity.x,0.1,10) * delta
+	#print('X: ', angular_velocity.x, " Y: ", angular_velocity.y, " Z: ", angular_velocity.z)
+	pass 
 
 
-func _on_area_3d_input_event(camera, event, position, normal, shape_idx):
+func _on_area_3d_input_event(camera, event, myposition, normal, shape_idx):
 	if event is InputEventMouseButton:
-		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed == true:
+		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed == true and CanClick == true:
+			CanClick = false
+			ClickTimer.start()
 			print("Pressed")
-			apply_impulse(Vector3.UP * 5, position)
+			Animator.play("Flip")
+			apply_impulse(Vector3.UP * force, position)
+	pass # Replace with function body.
+
+
+func _on_click_timer_timeout():
+	CanClick = true
 	pass # Replace with function body.
